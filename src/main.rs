e0c8b490e35b40135555cb362ddc09456b9e5ea1
Soham <sohamsinha2001@gmail.com>
2025-08-10 21:14:17 +0530

    use of borrowing

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,11 @@ fn main() {
 
     let rand_val = use_random();
     println!("{rand_val}");
+  //borrowing the string from the function
+    let s_cal = String::from("helllllllloooo");
+    //only taking the reference of the string. not the actual string is moving to the function. only pointing to the address of the string instance.
+    let s_cal2 = calculate_len_of_tupple(&s_cal);
+    println!("the size of the tupple is {s_cal2}");
    
 }
 pub fn takes_ownership(some_string: String){
@@ -44,11 +49,15 @@ pub fn gives_ownership () -> String{
 pub fn gives_and_takes_ownership(a_string:String)->String{
     a_string
 }
-pub fn calculate_length(s:String) -> (String, usize) {
+pub fn calculate_length(s: String) -> (String, usize) {
     let length: usize = s.len();
     (s,length)
 }
 pub fn use_random()->i32{
-    let a = rand::thread_rng().gen_range(1..=100);
+    let a: i32 = rand::thread_rng().gen_range(1..=100);
     a
+}
+//understanding borrowing
+pub fn calculate_len_of_tupple( s: &String)-> usize {
+    s.len()
 }
